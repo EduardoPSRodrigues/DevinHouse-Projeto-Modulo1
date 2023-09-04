651c95aa681e420d639f807c72e04db880ce4a67
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-09-04 10:40:51 -0300

    Atualização Dashboard

--- FitManage Tech/src/views/Dashboard/Dashboard.vue
+++ FitManage Tech/src/views/Dashboard/Dashboard.vue
@@ -2,16 +2,19 @@
 
 <template>
  <v-app>
+
+    <p>Bem vindo, {{ usuarioName }}</p>
+
       <v-container>
         <v-row>
           <v-col cols="6">
             <v-card>
               <v-card-title>Card 1</v-card-title>
               <v-card-text>
-                Conteúdo do Card 1.
+                10 Alunos
               </v-card-text>
               <v-card-actions>
-                <v-btn color="primary">Botão do Card 1</v-btn>
+                <v-btn color="primary">Adicionar</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -19,10 +22,10 @@
             <v-card>
               <v-card-title>Card 2</v-card-title>
               <v-card-text>
-                Conteúdo do Card 2.
+                10 Exercícios
               </v-card-text>
               <v-card-actions>
-                <v-btn color="primary">Botão do Card 2</v-btn>
+                <v-btn color="primary">Adicionar</v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
@@ -30,11 +33,49 @@
       </v-container>
     </v-app>
 </template>
+
+
 <script>
-export default {
-    
-}
-</script>
+  import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+        posts: [],
+        usuarioName: '',
+      }
+    },
+    mounted() {
+      this.loadPosts()
+      this.usuarioName = localStorage.getItem('usuario_name');
+    },
+    methods: {
+      loadPosts() {
+        //Pegando o token no local storage
+        const token = localStorage.getItem('usuario_token')
+  
+        axios({
+          url: 'http://localhost:3000/dashboard',
+          method: 'GET',
+          //A rota é privada e tenho que passar o token para conseguir fazer o get
+          headers: {
+            Authorization: `Bearen ${token}`
+          }
+        })
+        //Como no get a intenção é pegar dados, então tenho que criar uma variavel para armazenar as informações do get, sendo que
+        //response.data é a resposta da minha requisição, posts é o nome da variavel que estão as informações no meu servidor
+          .then((response) => {
+            this.posts = response.data.posts
+          })
+          .catch(() => {
+            alert('deu ruim')
+          })
+      },
+    }
+  }
+  </script>
+
+
 <style>
     *{
         margin: 0;
