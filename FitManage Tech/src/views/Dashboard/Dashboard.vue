f2354f7aef940bc407a8644ed83382a46e541b7d
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-09-04 10:22:20 -0300

    Atualização dos arquivos

--- FitManage Tech/src/views/Dashboard/Dashboard.vue
+++ FitManage Tech/src/views/Dashboard/Dashboard.vue
@@ -1,9 +1,34 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 
 <template>
-    <div>
-        <h1>TELA DE DASHBOARD</h1>
-    </div>
+ <v-app>
+      <v-container>
+        <v-row>
+          <v-col cols="6">
+            <v-card>
+              <v-card-title>Card 1</v-card-title>
+              <v-card-text>
+                Conteúdo do Card 1.
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary">Botão do Card 1</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="6">
+            <v-card>
+              <v-card-title>Card 2</v-card-title>
+              <v-card-text>
+                Conteúdo do Card 2.
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary">Botão do Card 2</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app>
 </template>
 <script>
 export default {
