3ef0819484e20066e8ff43d061a9471ef44568d0
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-09-04 11:13:02 -0300

    Escopo da tela de Dashboard

--- FitManage Tech/src/views/Dashboard/Dashboard.vue
+++ FitManage Tech/src/views/Dashboard/Dashboard.vue
@@ -1,83 +1,85 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 
 <template>
- <v-app>
+    <v-app>
 
-    <p>Bem vindo, {{ usuarioName }}</p>
+        <p>Bem vindo, {{ userName }}</p>
 
-      <v-container>
-        <v-row>
-          <v-col cols="6">
-            <v-card>
-              <v-card-title>Card 1</v-card-title>
-              <v-card-text>
-                10 Alunos
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="primary">Adicionar</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-          <v-col cols="6">
-            <v-card>
-              <v-card-title>Card 2</v-card-title>
-              <v-card-text>
-                10 Exercícios
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="primary">Adicionar</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
+        <v-container>
+            <v-row>
+                <v-col cols="6">
+                    <v-card>
+                        <!-- <v-card-title>Card 1</v-card-title> -->
+                        <v-card-text>
+                            {{ responseDashboard.amount_students }} Aluno(s)
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn color="primary">Adicionar</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+                <v-col cols="6">
+                    <v-card>
+                        <!-- <v-card-title>Card 2</v-card-title> -->
+                        <v-card-text>
+                            {{ responseDashboard.amount_exercises }} Exercício(s)
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn color="primary">Adicionar</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-app>
 </template>
 
 
 <script>
-  import axios from 'axios'
-  
-  export default {
+import axios from 'axios'
+
+export default {
     data() {
-      return {
-        posts: [],
-        usuarioName: '',
-      }
+        return {
+            userName: '',
+
+            responseDashboard: {
+                //amount_students: "",
+                // amount_exercises: ""
+            },
+
+        }
     },
     mounted() {
-      this.loadPosts()
-      this.usuarioName = localStorage.getItem('usuario_name');
+        this.loadDashboard()
+        this.userName = localStorage.getItem('user_name');
     },
     methods: {
-      loadPosts() {
-        //Pegando o token no local storage
-        const token = localStorage.getItem('usuario_token')
-  
-        axios({
-          url: 'http://localhost:3000/dashboard',
-          method: 'GET',
-          //A rota é privada e tenho que passar o token para conseguir fazer o get
-          headers: {
-            Authorization: `Bearen ${token}`
-          }
-        })
-        //Como no get a intenção é pegar dados, então tenho que criar uma variavel para armazenar as informações do get, sendo que
-        //response.data é a resposta da minha requisição, posts é o nome da variavel que estão as informações no meu servidor
-          .then((response) => {
-            this.posts = response.data.posts
-          })
-          .catch(() => {
-            alert('deu ruim')
-          })
-      },
+        loadDashboard() {
+            //Pegando o token no local storage
+            const token = localStorage.getItem('user_token')
+
+            axios({
+                url: 'http://localhost:3000/dashboard',
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearen ${token}`
+                }
+            })
+                .then((response) => {
+                    this.responseDashboard = response.data
+                })
+                .catch(() => {
+                    alert('Houve uma falha ao carregar as informações.')
+                })
+        },
     }
-  }
-  </script>
+}
+</script>
 
 
 <style>
-    *{
-        margin: 0;
-    }
+* {
+    margin: 0;
+}
 </style>
